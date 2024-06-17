3a6c63bf515400ecee78d94b5668a6e49f04ad2e
Zoroark211 <123588204+Zoroark211@users.noreply.github.com>
2024-06-16 17:13:41 -0700

    Delete FilestackPython directory

--- FilestackPython/ctDataCount.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-dataFrame = pd.read_excel('CTDataTable.xlsx')
-
-count = dataFrame['Study.ProtocolSection.IdentificationModule.NCTId'].nunique()
-id = dataFrame['Study.ProtocolSection.IdentificationModule.NCTId'].value_counts()
-
-print('Number of Unique IDs in CTData: ', count)
-print('List of Unique IDs ', id)
